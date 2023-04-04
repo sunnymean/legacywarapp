370e718b058138d99e5aef905c497bd92de17ca0
sunnymean <sryu@hist.co.kr>
2023-04-04 15:03:24 +0900

    initial commit

---
+++ src/main/java/legacy/spring/oldspringapps/OldspringappsApplication.java
@@ -0,0 +1,13 @@
+package legacy.spring.oldspringapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldspringappsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldspringappsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/legacy/spring/oldspringapps/OldspringappsApplicationTests.java
@@ -0,0 +1,13 @@
+package legacy.spring.oldspringapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldspringappsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
